5ff4a3d606a99c00c688cdf463e5fa78da1dc0bd
sjcco <73748496+sjcco@users.noreply.github.com>
2021-09-03 12:35:16 -0500

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: '',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: true,
+              modules: {
+                namedExport: true,
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    htmlPlugin,
+    new MiniCssExtractPlugin(),
+  ],
+};
